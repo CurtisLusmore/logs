1b7b09507c26b25e20bd8a07d52ed1985834ebe8
Curtis Lusmore <curtis@lusmo.re>
2020-08-01 22:55:56 +1000

    Implement Get function
    This function gets all LogEntity records in the LogEntries table

--- Get.cs
+++ Get.cs
@@ -1,11 +1,14 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage.Table;
 using Newtonsoft.Json;
 
 namespace logs
@@ -14,22 +17,26 @@ namespace logs
     {
         [FunctionName("Get")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            [Table("LogEntities")] CloudTable table,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            var logs = new List<LogEntity>();
+            TableContinuationToken token = default;
+            do {
+                var query = new TableQuery<LogEntity>();
+                var results = await table.ExecuteQuerySegmentedAsync(query, token);
+                logs.AddRange(results);
+                token = results.ContinuationToken;
+            } while (token != default);
 
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            return new OkObjectResult(
+                logs.Select(result => new
+                    {
+                        pathname = result.PartitionKey.Replace("|", "/"),
+                        referrer = result.Referrer,
+                        datetime = result.DateTime
+                    }));
         }
     }
 }
